b67183f72ec9ff0125a3089ef3ae30f4680e30ba
katila2011 <69424310+katila2011@users.noreply.github.com>
2024-09-24 14:37:20 +0100

    Assignment2309-commit

---
+++ src/main/java/classAssignment/Assignment2309.java
@@ -0,0 +1,25 @@
+package classAssignment;
+import java.util.Scanner;
+
+public class Assignment2309 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        System.out.println("Enter your mother's maiden name");
+        String maidenName = scanner.nextLine();
+        String maidenName1 = maidenName.toUpperCase();
+        int numVowels = maidenName1.length();
+        String reverse = "";
+
+        for (int i = maidenName1.length() - 1; i >= 0; i--) {
+            reverse = reverse + maidenName1.charAt(i);
+        }
+        System.out.println("The reverse of your maiden name is " + reverse);
+        if (reverse!=maidenName){
+            System.out.println("Your mother's maiden name is not a palindrome");
+        }
+        else{
+            System.out.println(reverse);
+        }
+    }
+}
